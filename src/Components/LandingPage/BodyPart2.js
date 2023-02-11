f4741e544ef31a392f6af48830deb1484be2fe3e
norahwinda <norahwinda48@gmail.com>
2023-02-11 15:11:25 +0300

    updating the image path

--- src/Components/LandingPage/BodyPart2.js
+++ src/Components/LandingPage/BodyPart2.js
@@ -5,17 +5,17 @@ function BodyPart2() {
   return (
     <div className='bodypart2'>
       <div className='bodypart2__top'>
-        <img src={"./Images/Rectangle1.png"} alt=''/>
+        <img src={"./images/Rectangle1.png"} alt=''/>
         <h3>Studio</h3>
         <p>You will find best photo studios here</p>
       </div>
       <div className='bodypart2__center'>
-         <img src={"./Images/Rectangle2.png"} alt=''/>
+         <img src={"./images/Rectangle2.png"} alt=''/>
          <h3>Events</h3>
          <p>Find events photographs</p>
       </div>
       <div className='bodypart2__bottom'>
-          <img src={"./Images/Rectangle3.png"}  alt=''/>
+          <img src={"./images/Rectangle3.png"}  alt=''/>
           <h3>Commercial</h3>
           <p>Find photographs that will help sell you products</p>
       </div>
