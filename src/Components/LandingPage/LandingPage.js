5f36ea97fe67e39d4b745104b64247c5ae86c8b2
norahwinda <norahwinda48@gmail.com>
2023-02-12 07:52:45 +0300

    styling

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -2,10 +2,11 @@ import React from 'react'
 import BodyPart1 from './BodyPart1'
 import BodyPart2 from './BodyPart2'
 import BodyPart3 from './BodyPart3'
+import './LandingPage.css'
 
 function LandingPage() {
   return (
-    <div>
+    <div className='landingpage'>
       <BodyPart1 />
       <BodyPart2 />
       <BodyPart3 />
