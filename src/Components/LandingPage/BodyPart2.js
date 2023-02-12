2a2c6220336b39b24825196a851a9a273a48403e
norahwinda <norahwinda48@gmail.com>
2023-02-12 10:52:56 +0300

    Landing page debug

--- src/Components/LandingPage/BodyPart2.js
+++ src/Components/LandingPage/BodyPart2.js
@@ -7,17 +7,17 @@ import Rectangle3 from '../../images/Rectangle3.png'
 function BodyPart2() {
   return (
     <div className='bodypart2'>
-      <div className='bodypart2__top'>
+      <div className='bodypart2-top'>
         <img src={Rectangle1} alt=''/>
         <h3>Studio</h3>
         <p>You will find best photo studios here</p>
       </div>
-      <div className='bodypart2__center'>
+      <div className='bodypart2-center'>
          <img src={Rectangle2} alt=''/>
          <h3>Events</h3>
          <p>Find events photographs</p>
       </div>
-      <div className='bodypart2__bottom'>
+      <div className='bodypart2-bottom'>
           <img src={Rectangle3}  alt=''/>
           <h3>Commercial</h3>
           <p>Find photographs that will help sell you products</p>
