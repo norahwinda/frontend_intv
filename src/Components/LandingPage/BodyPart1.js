cf1be86d3a14f9521ab0e5a4f427ad3eb7e8a130
norahwinda <norahwinda48@gmail.com>
2023-02-12 10:39:12 +0300

    bodypart1.css debug

--- src/Components/LandingPage/BodyPart1.js
+++ src/Components/LandingPage/BodyPart1.js
@@ -5,12 +5,12 @@ import body from '../../images/body1.png'
 function BodyPart1() {
   return (
     <div className='bodypart1'>
-      <div className='bodypart1__left'>
+      <div className='bodypart1-left'>
         <h1>Photography for all your products needs</h1>
         <p>You will meet different people from every part of the world with excellent skills for your products</p>
         <button>Let's start</button>
       </div>
-      <div className='bodypart1__right'>
+      <div className='bodypart1-right'>
             <img  src={body} alt=''/>
       </div>
     </div>
