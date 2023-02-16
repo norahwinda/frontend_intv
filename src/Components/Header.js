b2ad729a9d059d68236b96b669f844971b621dd6
norahwinda <norahwinda48@gmail.com>
2023-02-16 10:11:33 +0300

    Cleaning lint code base

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -27,13 +27,13 @@ function Header () {
     <div className='header'>
       <h3>Photography</h3>
       <div className='header-right'>
-        {user
-        ? <div className='headerright-signout'>
-          <button onClick={handleHomeClick}>HOME</button>
-          <button onClick={LogOut}>SIGN OUT</button>
-          <Avatar src={user.photoURL} className='header-avatar' />
-        </div>
-        : <button onClick={() => navigate('/frontend_intv/signup')}>SIGN IN</button>}
+        {
+          user? <div className='headerright-signout'>
+                  <button onClick={handleHomeClick}>HOME</button>
+                  <button onClick={LogOut}>SIGN OUT</button>
+                  <Avatar src={user.photoURL} className='header-avatar' />
+                </div>
+          : <button onClick={() => navigate('/frontend_intv/signup')}>SIGN IN</button>}
       </div>
     </div>
   )
